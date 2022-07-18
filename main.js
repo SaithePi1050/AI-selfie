aff02038f03763332cb4230b38544b45d0e76f4d
SaithePi <89380734+SaithePi1050@users.noreply.github.com>
2022-07-18 18:16:59 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,24 @@ recongnition.onresult = function(event)
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("text_area").innerHTML = content;
+    speak();
+    Webcam.attach(camera);
 }
+
+function speak() 
+{
+    var API1 = window.speechSynthesis;
+    var speakdata = document.getElementById("text_area").value;
+    var savevalue  = new SpeechSynthesisUtterance(speakdata);
+    API1.speak(savevalue);
+}
+
+camera = document.getElementById("camera");
+
+Webcam.set
+({
+    width : 400,
+    height : 400,
+    image_format : 'png', 
+    png_quality : 90,
+})
