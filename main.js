faa99933b4153deaf673ec64e9bb472603dcd592
SaithePi <89380734+SaithePi1050@users.noreply.github.com>
2022-07-25 18:48:12 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -12,14 +12,23 @@ recongnition.onresult = function(event)
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("text_area").innerHTML = content;
-    speak();
+        if (content == "take my selfie") 
+        {
+            speak(); 
+        }
     Webcam.attach(camera);
+    setTimeout(
+        function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 function speak() 
 {
     var API1 = window.speechSynthesis;
-    var speakdata = document.getElementById("text_area").value;
+    var speakdata = "Taking your selfie in 5 seconds";
     var savevalue  = new SpeechSynthesisUtterance(speakdata);
     API1.speak(savevalue);
 }
@@ -33,3 +42,21 @@ Webcam.set
     image_format : 'png', 
     png_quality : 90,
 })
+
+function take_snapshot() 
+{
+    Webcam.snap(
+        function (data_uri) 
+        {
+            document.getElementById("selfie").innerHTML = '<img id = "selfie_image" src = " '+ data_uri +' ">';
+        }
+    );
+}
+
+function save() 
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
